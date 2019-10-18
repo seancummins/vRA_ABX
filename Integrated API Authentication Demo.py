e900951379968dad70875868840fb89fb80aeece
Sean Cummins <sean@scummins.com>
2019-10-18 11:37:07 -0400

    Action Version: 1

---
+++ Integrated API Authentication Demo.py
@@ -0,0 +1,16 @@
+import json
+import pprint
+
+def handler(context, inputs):
+    # Use the request method of the context object to call the vRAC API with automatic authentication
+    
+    payload = ""   # context.request requires a payload; for a GET, we'll pass an empty payload
+    url = "/deployment/api/deployments"
+
+    apicall = context.request(url, 'GET', payload)   # issue API call
+    result = apicall['content'].decode('UTF-8')      # Decode byte literal into JSON string
+    content = json.loads(result)                     # Load JSON string into JSON object
+    
+    pprint.pprint(content['content'])                # Pretty print JSON object
+
+    return content
